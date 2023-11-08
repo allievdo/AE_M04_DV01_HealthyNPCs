b9046dd1c2e2eee781ef3f25d7e6df1a2585ca7d
allikitten <allison.evdokimo@cox.net>
2023-11-08 12:03:27 -0700

    AddedScripts

---
+++ SRPNPC Project/Assets/Scripts/NumberOfHitsHealth.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberOfHitsHealth : MonoBehaviour, IHealth
+{
+    [SerializeField]
+    private int healthInHits = 5;
+
+    [SerializeField]
+    private float invulnerabilityTimeAfterEachHit = 5f;
+
+    private int hitsRemaining;
+    private bool canTakeDamage = true;
+
+    public event Action<float> OnHPPctChanged = delegate { };
+    public event Action OnDied =delegate { };
+
+    public float CurrentHpPct
+    {
+        get { return (float) hitsRemaining / (float) healthInHits;}
+    }
+
+    private void Start()
+    {
+        hitsRemaining = healthInHits;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (canTakeDamage)
+        {
+            StartCoroutine(InvulnerabilityTimer());
+
+            hitsRemaining--;
+
+            OnHPPctChanged(CurrentHpPct);
+
+            if (hitsRemaining <= 0)
+                OnDied();
+        }
+    }
+
+    private IEnumerator InvulnerabilityTimer()
+    {
+        canTakeDamage = false;
+        yield return new WaitForSeconds(invulnerabilityTimeAfterEachHit);
+        canTakeDamage = true;
+    }
+}
